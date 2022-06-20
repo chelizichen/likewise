52675af0178bcea5b979b07d06baa6354b96637e
Leemulus <1347290221@qq.com>
2022-06-20 14:34:21 +0800

    密码加密

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,6 +1,10 @@
 import { BadRequestException, Injectable } from "@nestjs/common";
 import { UserPipe } from "./user.pipe";
 import { UsersService } from "./users.service";
+import { randomBytes,scrypt as _scrypt } from 'crypto' // 密码 加密 解密的方法
+import { promisify } from 'util'
+
+let scrypt = promisify(_scrypt)
 
 @Injectable()   
 export class AuthService{
@@ -13,18 +17,24 @@ export class AuthService{
         if(user){
             throw new BadRequestException("用户已存在")
         }else{
-            return await this.UserService.singUp({email,password,userName})
+            const salt = randomBytes(8).toString('hex')
+            const hash =  ( await scrypt(password,salt,32) ) as Buffer
+            const result = salt + '.' + hash.toString('hex')
+            const user = await this.UserService.singUp({email,password:result,userName})
+            return user
         }
-        // return await this.UserService.create({email,password})
     }
 
     // 登陆验证
     async signIn(body:UserPipe){
         const {email,password} = body
         const user = await this.UserService.findByEmail({email})
-        console.log(user);
+
         if(user){
-            if(user.password === password){
+            // 解密
+            const [salt,storedHash] = user.password.split('.');
+            const hash = ( await scrypt(password,salt,32) ) as Buffer
+            if(storedHash === hash.toString('hex')){
                 return {
                     Code:201,
                     msg:'登陆成功',
